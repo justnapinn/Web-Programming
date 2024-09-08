2eccbc566888a72944d9d9dd441d7a9006229659
justnapinn <jinnaphatpansri@gmail.com>
2024-09-08 16:46:17 +0700

    finish-JS-lab

--- lab/Lab5/Lab5-2/jsform.js
+++ lab/Lab5/Lab5-2/jsform.js
@@ -1,15 +1,45 @@
-function flNameFun(){
+
+function progressFun(){
+
+    // Name function
     const fname = document.getElementById("fnameinp").value;
     const lname = document.getElementById("lnameinp").value;
-    const fullName = fname + ", " + lname;
-  
-    document.getElementById("fullName").value = fullName;
-}
-function weeklyEarnFun(){
+    if(fname === "" || lname === ""){
+        alert("Please fill in both First Name and Last Name.");
+        return false;
+    }
+    else{
+        const fullName = fname + ", " + lname;
+        document.getElementById("fullName").value = fullName;
+    }
+
+    // -----------------------------------------------------------
+
+    // Salary function
+
+    weekList = [
+        parseFloat(document.getElementById("valueOfMon").value) ,
+        parseFloat(document.getElementById("valueOfTue").value) ,
+        parseFloat(document.getElementById("valueOfWed").value) ,
+        parseFloat(document.getElementById("valueOfThu").value) ,
+        parseFloat(document.getElementById("valueOfFri").value) ,
+        parseFloat(document.getElementById("valueOfSat").value) ,
+        parseFloat(document.getElementById("valueOfSun").value) 
+    ]
+
+    let sumHourPerWeek = 0;
+    let salaryPerDay = parseFloat(document.getElementById("hourlySalary").value);
+    let salaryPerWeek = 1;
+    for (let i = 0; i<7; i++){
+        if (weekList[i] > 8){
+            alert("You can't work more than 8 hours.")
+            return false;
+        }
+        else{
+            sumHourPerWeek += weekList[i];
+        }
+    }
+    salaryPerWeek = salaryPerDay * sumHourPerWeek;
+    document.getElementById("weekEarn").value = salaryPerWeek;
 
 }
-
-function calculate() {
-    flNameFun();
-    weeklyEarnFun();
-  }
